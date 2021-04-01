5e61b9565277fd0de816b1941ac7f7496f1d45a6
chut-ai <louis.hemadou@eleves.enpc.fr>
2021-04-01 16:01:24 +0200

    Test acc

--- meta/data/task_generator.py
+++ meta/data/task_generator.py
@@ -7,8 +7,9 @@ import torch.nn as nn
 import numpy as np
 from PIL import Image
 
+
 def open_json(domain):
-    
+
     json_path = "/home/louishemadou/VisDA_encoded/" + domain + ".json"
 
     with open(json_path, "r") as f:
@@ -16,8 +17,9 @@ def open_json(domain):
     f.close()
     return data
 
+
 def squeeze(labels):
-    
+
     items = []
     for label in labels:
         if label not in items:
@@ -30,44 +32,67 @@ def squeeze(labels):
         squeezed.append(items.index(label))
     return squeezed
 
-def make_one_task(data, n_class, n_spt, n_qry):
 
-    n_instance = n_spt + n_qry
-    
-    possible_class = []
-    for i in range(1, 346):
-        if len(data[str(i)]) >= n_instance:
-            possible_class.append(i)
 
-    chosen_labels = np.random.choice(possible_class, n_class, False)
 
-    instances_labels = []
+class VisdaTask:
+    def __init__(self, domain, n_class, n_qry, n_spt):
+        self.domain = domain
+        self.data = open_json(domain)
 
-    for label in chosen_labels:
-        indexes = np.random.choice(
-            range(len(data[str(label)])), n_instance, False)
-        for index in indexes:
-            instance = torch.Tensor(data[str(label)][index])
-            instances_labels.append([instance, label])
-    random.shuffle(instances_labels)
+        self.n_class = n_class
+        self.n_qry = n_qry
+        self.n_spt = n_spt
+        possible_class = []
+        for i in range(1, 346):
+            if len(self.data[str(i)]) >= n_qry + n_spt:
+                possible_class.append(i)
+        n_test_class = int(len(possible_class)/2)
+        n_train_class = len(possible_class) - n_test_class
 
-    instances = [elem[0] for elem in instances_labels]
-    labels = squeeze([elem[1] for elem in instances_labels])
-    x_spt = torch.stack(instances[:n_spt*n_class], 0)
-    x_qry = torch.stack(instances[n_spt*n_class:], 0)
-    y_spt = torch.Tensor(labels[:n_spt*n_class])
-    y_qry = torch.Tensor(labels[n_spt*n_class:])
+        self.train_class = list(np.random.choice(
+            possible_class, n_train_class, False))
+        self.test_class = [
+            x for x in possible_class if x not in self.train_class]
 
-    return x_spt, x_qry, y_spt, y_qry
+    def task(self, mode):
 
-def make_tasks(domain, n_class, n_spt, n_qry, task_bsize, n_batch):
-    
-    data = open_json(domain)
+        if mode == "train":
+            class_list = self.train_class
+        elif mode == "test":
+            class_list = self.test_class
+        else:
+            print('mode has to be "train" or "test"')
 
-    tasks = [[make_one_task(data, n_class, n_spt, n_qry) for i in range(task_bsize)] for _ in range(n_batch)]
+        chosen_labels = np.random.choice(class_list, self.n_class, False)
 
-    return tasks
+        chosen_data = []
+
+        for label in chosen_labels:
+            indexes = np.random.choice(
+                range(len(self.data[str(label)])), self.n_qry+self.n_spt, False)
+            for index in indexes:
+                instance = torch.Tensor(self.data[str(label)][index])
+                chosen_data.append([instance, label])
+        random.shuffle(chosen_data)
+
+        instances = [elem[0] for elem in chosen_data]
+        labels = squeeze([elem[1] for elem in chosen_data])
+        x_spt = torch.stack(instances[:self.n_spt*self.n_class], 0)
+        x_qry = torch.stack(instances[self.n_spt*self.n_class:], 0)
+        y_spt = torch.Tensor(labels[:self.n_spt*self.n_class])
+        y_qry = torch.Tensor(labels[self.n_spt*self.n_class:])
+
+        return x_spt, x_qry, y_spt, y_qry
+
+    def task_batch(self, mode, task_bsize):
+
+        tasks = [self.task(mode) for _ in range(task_bsize)]
+
+        return tasks
+
 
 if __name__ == "__main__":
-    data = open_json("real")
-    a = make_one_task(data, 10, 50, 50)
+    visdatask = VisdaTask("clipart", 10, 10, 10)
+    task = visdatask.task_batch("train", 10)
+    print(len(task[0]))
--- meta/main.py
+++ meta/main.py
@@ -1,5 +1,7 @@
-from maml.meta.data.task_generator import make_tasks
+from maml.meta.data.task_generator import VisdaTask
 from maml.meta.model import DenseNet
+from maml.meta.meta_train import meta_train
+from maml.meta.meta_test import meta_test
 import matplotlib.pyplot as plt
 import torch.optim as optim
 import torch
@@ -11,60 +13,42 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 net = DenseNet().to(device)
 net.train()
 
-print("Making tasks ...")
 
 n_class = 10
 n_spt = 10
 n_qry = 90
 task_bsize = 10
 n_batch = 500
+n_iter_inner_loop = 10
 
-tasks = make_tasks("real", n_class, n_spt, n_qry, task_bsize, n_batch)
+print("Loading data ...")
+
+visda = VisdaTask("real", n_class, n_qry, n_spt)
 
 print("Done !")
 
-n_iter_inner_loop = 20
 meta_opt = optim.Adam(net.parameters(), lr=0.001)
-qry_accs = []
+qry_accs_train = []
+qry_accs_test = []
 
 print("Training ...")
 
-for i, task_batch in enumerate(tasks):
-
-    message = "Task batch {}/{} ({:.1f}%) \r".format(i, len(tasks), 100*i/len(tasks))
+for i in range(n_batch):
+    
+    qry_train_acc = meta_train(visda, net, meta_opt, n_iter_inner_loop, task_bsize, device)
+    qry_accs_train.append(qry_train_acc)
+    qry_test_acc = meta_test(visda, net, n_iter_inner_loop, task_bsize, device)
+    qry_accs_test.append(qry_test_acc)
+    
+    message = "Task batch {}/{} ({:.1f}%) \r".format(i, n_batch, 100*i/n_batch)
     print(message, sep=" ", end="", flush=True)
 
 
-    inner_opt = optim.SGD(net.parameters(), lr=0.1)
-
-    task_qry_acc = 0
-    meta_opt.zero_grad()
-
-    for task in task_batch:
-        x_spt, x_qry, y_spt, y_qry = task
-        x_spt = x_spt.to(device)
-        y_spt = y_spt.to(device, dtype=torch.int64)
-        x_qry = x_qry.to(device)
-        y_qry = y_qry.to(device, dtype=torch.int64)
-        
-
-        with higher.innerloop_ctx(net, inner_opt, device, copy_initial_weights=False) as (fnet, diffopt):
-            for _ in range(n_iter_inner_loop):
-                spt_logits = fnet(x_spt)
-                spt_loss = F.cross_entropy(spt_logits, y_spt)
-                diffopt.step(spt_loss)
-
-            qry_logits = fnet(x_qry)
-            task_qry_acc += (qry_logits.argmax(dim=1) ==
-                    y_qry).sum().item()/(y_qry.size()[0]*len(task_batch))
-            qry_loss = F.cross_entropy(qry_logits, y_qry)
-            qry_loss.backward()
-    qry_accs.append(task_qry_acc)
-    meta_opt.step()
-
-X = range(len(qry_accs))
+X = range(len(qry_accs_train))
 plt.figure()
-plt.scatter(X, qry_accs)
-plt.xlabel("Number of tasks")
+plt.scatter(X, qry_accs_train, color="k", label="train")
+plt.scatter(X, qry_accs_test, color="r", label="test")
+plt.legend()
+plt.xlabel("Number of task batchs")
 plt.ylabel("Accuracy of query")
 plt.show()
